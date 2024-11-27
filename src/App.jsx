44f937bf4af3335d7c4e71b947dc9a7e1feb9d62
adelaida <adelaidaescobar@oakland.edu>
2024-11-26 19:38:29 -0500

    test

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from '../../React_Demo/src/components/Header/Header';
+import Hero from './components/Header/Hero'; // this was just inserted
+
+function App() {
+  // const [count, setCount] = useState(0) 51:39
+
+  return (
+    <>
+      <Header /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// file imports that you need -- css, media images
+import "./Header.css";
+import logoImage from "../../assets/FELV-cat.jpg";
+
+// declare the component
+function Header() {
+  return (
+    <>
+      <img src={logoImage} className="header-img" alt="logo for website" />
+      <h2 className="header-title">Welcome to the Meme Generator App</h2>
+      <h4 className="header-text">A Dynamic Web App using React</h4>
+    </>
+  );
+}
+
+// export statement
+export default Header;
